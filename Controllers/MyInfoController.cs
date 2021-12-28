dd7f0e5d272217ff622e36810865c4b92493f44e
thuw12 <huynhminhthu12@gmail.com>
2021-12-29 01:11:55 +0700

    THIEU phan update anh cho MYINFO

--- Controllers/MyInfoController.cs
+++ Controllers/MyInfoController.cs
@@ -13,80 +13,103 @@ public class MyInfoController : ControllerBase
     [Route("api/update-myinfo"), HttpPost]
     public JsonResult UpdateMyInfo()
     {
-        var reader = new StreamReader(HttpContext.Request.Body);
-        var body = reader.ReadToEnd();
-        dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
-
-        string Email = (string)data.Email;
-
-        var context = new DBContext();
-
-        var user = (from u in context.users
-                    where u.Email == Email
-                    select u).FirstOrDefault();
-
-        user.Fullname = (string)data.Fullname;
-        user.Date_of_birth = (string)data.Date_of_birth;
-        user.City_id = (string)data.City_id;
-        user.Phone = (int)data.Phone;
-        user.School = (string)data.School;
-        user.Facebook = (string)data.Facebook;
-        user.Bio = (string)data.Bio;
-
-        context.Update(user);
-        context.SaveChanges();
-
-        var result = new
+        try
         {
-            status = 200,
-            message = "User Updated Successfully",
-            User = user,
-        };
-        return new JsonResult(result);
+            var reader = new StreamReader(HttpContext.Request.Body);
+            var body = reader.ReadToEnd();
+            dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
+
+            string Email = (string)data.Email;
+
+            var context = new DBContext();
+            var user = (from u in context.users
+                        where u.Email == Email
+                        select u).FirstOrDefault();
+
+            user.Fullname = (string)data.Fullname;
+            user.Date_of_birth = (string)data.Date_of_birth;
+            user.City_id = (string)data.City_id;
+            user.Phone = (int)data.Phone;
+            user.School = (string)data.School;
+            user.Facebook = (string)data.Facebook;
+            user.Bio = (string)data.Bio;
+
+            context.Update(user);
+            context.SaveChanges();
+
+            var result = new
+            {
+                status = 200,
+                message = "User Updated Successfully",
+                User = user,
+            };
+            return new JsonResult(result);
+        }
+        catch (Exception ex)
+        {
+            var result = new
+            {
+                status = 404,
+                message = ex.InnerException,
+            };
+            return new JsonResult(result);
+        }
     }
 
 
     [Route("api/update-password"), HttpPut]
     public JsonResult UpdatePassword()
     {
-        var reader = new StreamReader(HttpContext.Request.Body);
-        var body = reader.ReadToEnd();
-        dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
-
-        int Userid = (int)data.User_id;
-
-        string currentPassword = (string)data.currentPassword;
-        string newPassword = (string)data.newPassword;
-
-        var context = new DBContext();
-
-        var acc = (from a in context.accounts
-                   where a.User_id == Userid
-                   select a).FirstOrDefault();
-
-        if (acc != null && BCrypt.Net.BCrypt.Verify(currentPassword, acc.Pwd))
+        try
         {
-            acc.Pwd = BCrypt.Net.BCrypt.HashPassword(newPassword);
-            context.Update(acc);
-            context.SaveChanges();
+            var reader = new StreamReader(HttpContext.Request.Body);
+            var body = reader.ReadToEnd();
+            dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
 
-            var result = new
+            int Userid = (int)data.User_id;
+
+            string currentPassword = (string)data.currentPassword;
+            string newPassword = (string)data.newPassword;
+
+            var context = new DBContext();
+
+            var acc = (from a in context.accounts
+                       where a.User_id == Userid
+                       select a).FirstOrDefault();
+
+            if (acc != null && BCrypt.Net.BCrypt.Verify(currentPassword, acc.Pwd))
             {
-                status = 200,
-                message = "Password Updated Successfully",
-                Account = acc,
-            };
-            return new JsonResult(result);
+                acc.Pwd = BCrypt.Net.BCrypt.HashPassword(newPassword);
+                context.Update(acc);
+                context.SaveChanges();
+
+                var result = new
+                {
+                    status = 200,
+                    message = "Password Updated Successfully",
+                    Account = acc,
+                };
+                return new JsonResult(result);
+            }
+            else
+            {
+                var result = new
+                {
+                    status = 404,
+                    message = "Fails",
+                };
+                return new JsonResult(result);
+            }
         }
-        else
+        catch (Exception ex)
         {
             var result = new
             {
                 status = 404,
-                message = "Fails",
+                message = ex.InnerException,
             };
             return new JsonResult(result);
         }
-    }
 
+    }
 }
--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -13,65 +13,89 @@ public class ProfileController : ControllerBase
     [Route("get-city"), HttpPost]
     public JsonResult GetCity()
     {
-        var reader = new StreamReader(HttpContext.Request.Body);
-        var body = reader.ReadToEnd();
-        dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
-
-        int Cityid = (int)data.City_id;
-        var context = new DBContext();
-
-        var city = from c in context.cities
-                   where c.City_id == Cityid
-                   select c;
-
-        var result = new
+        try
         {
-            status = 200,
-            city = city,
-        };
-        return new JsonResult(result);
+            var reader = new StreamReader(HttpContext.Request.Body);
+            var body = reader.ReadToEnd();
+            dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
+
+            int Cityid = (int)data.City_id;
+            var context = new DBContext();
+
+            var city = from c in context.cities
+                       where c.City_id == Cityid
+                       select c;
+
+            var result = new
+            {
+                status = 200,
+                city = city,
+            };
+            return new JsonResult(result);
+        }
+        catch (Exception ex)
+        {
+            var result = new
+            {
+                status = 404,
+                message = ex.InnerException,
+            };
+            return new JsonResult(result);
+        }
     }
 
 
     [Route("get-course-item"), HttpPost]
     public JsonResult GetProfileCourseItem()
     {
-        var reader = new StreamReader(HttpContext.Request.Body);
-        var body = reader.ReadToEnd();
-        dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
-        int Userid = (int)data.User_id;
-        var context = new DBContext();
-
-        var courses = (from les in context.lessons
-                       join l in context.learnings on les.Lesson_id equals l.Lesson_id
-                       join cp in context.course_chapters on les.Chapter_id equals cp.Course_chapter_id
-                       join c in context.courses on cp.Course_id equals c.Course_id
-                       join u in context.users on c.Author_id equals u.User_id
-                       where l.User_id == Userid
-                       orderby l.Learn_time descending
-                       select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id }).Distinct().Take(3);
-
-        var learntCourse = from c in context.courses
-                           join e in context.enrollments on c.Course_id equals e.Course_id
-                           join u in context.users on c.Author_id equals u.User_id
-                           where u.User_id == Userid
-                           where c.Course_state == "Công khai"
-                           select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id };
-
-        var uppedCourse = from c in context.courses
-                          join u in context.users on c.Author_id equals u.User_id
-                          where c.Author_id == Userid
-                          where c.Course_state == "Công khai"
-                          select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id };
-
-        var result = new
+        try
         {
-            status = 200,
-            courses = courses,
-            learntCourse = learntCourse,
-            uppedCourse = uppedCourse
-        };
-        return new JsonResult(result);
+            var reader = new StreamReader(HttpContext.Request.Body);
+            var body = reader.ReadToEnd();
+            dynamic? data = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(body);
+            int Userid = (int)data.User_id;
+            var context = new DBContext();
+
+            var courses = (from les in context.lessons
+                           join l in context.learnings on les.Lesson_id equals l.Lesson_id
+                           join cp in context.course_chapters on les.Chapter_id equals cp.Course_chapter_id
+                           join c in context.courses on cp.Course_id equals c.Course_id
+                           join u in context.users on c.Author_id equals u.User_id
+                           where l.User_id == Userid
+                           orderby l.Learn_time descending
+                           select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id }).Distinct().Take(3);
+
+            var learntCourse = from c in context.courses
+                               join e in context.enrollments on c.Course_id equals e.Course_id
+                               join u in context.users on c.Author_id equals u.User_id
+                               where u.User_id == Userid
+                               where c.Course_state == "Công khai"
+                               select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id };
+
+            var uppedCourse = from c in context.courses
+                              join u in context.users on c.Author_id equals u.User_id
+                              where c.Author_id == Userid
+                              where c.Course_state == "Công khai"
+                              select new { c.Course_id, c.Course_name, c.Img, u.Fullname, u.User_id };
+
+            var result = new
+            {
+                status = 200,
+                courses = courses,
+                learntCourse = learntCourse,
+                uppedCourse = uppedCourse
+            };
+            return new JsonResult(result);
+        }
+        catch (Exception ex)
+        {
+            var result = new
+            {
+                status = 404,
+                message = ex.InnerException,
+            };
+            return new JsonResult(result);
+        }
     }
 
 
